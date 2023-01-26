da97a6f8fe61a0b0f8e29abc320528977c00853c
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2023-01-27 00:53:58 +0100

    JH-13 moved old components to legacy folder

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,44 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [
-    require('@codelab/tailwind-config/base.js')
+  presets: [require('@codelab/tailwind-config/base')],
+  content: [
+    './src/components/**/*.{tsx}',
+    './src/legacy/components/**/*.{tsx}',
   ],
-  // Customizations specific to this project would go here
   theme: {
-
-    //CodeLab main colors
-    colors: {
-      //Light theme
-      'primary': '#3b50df',
-      'secondary': '#7286d3',
-      'success': '#19d16f',
-      'info': '#7e33e0',
-      'danger': '#fb2448',
-      'light': '#f9f8f9',
-      'warning': '#FDB400',
-
-      //Dark theme
-      'primary-dark': '#3b50df',
-      'secondary-dark': '#7286d3',
-      'success-dark': '#19d16f',
-      'info-dark': '#7e33e0',
-      'danger-dark': '#fb2448',
-      'light-dark': '#f9f8f9',
-      'warning-dark': '#FDB400',
-
-    },
-    fontFamily: {
-      sans: ['Open Sans', 'sans-serif'],
-      sans: ['Graphik', 'sans-serif'],
-    },
-    extend: {
-      screens: {
-        'xs': '320px',
-        // => @media (min-width: 320px) { ... }
-      },
-      minHeight: {
-        48: '12rem',
-      }
-    }
+    extend: {},
   },
-}
+  plugins: [],
+};
